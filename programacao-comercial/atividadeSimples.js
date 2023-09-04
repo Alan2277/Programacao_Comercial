b254cb2a0fc136d72451243a16455cc59462f9a5
Alan2277 <121243339+Alan2277@users.noreply.github.com>
2023-09-03 22:36:17 -0300

    Update atividadeSimples.js

--- programacao-comercial/atividadeSimples.js
+++ programacao-comercial/atividadeSimples.js
@@ -1,182 +1,99 @@
 import {Text, View, StyleSheet, Image} from 'react-native';
-
-const imagem = 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Vasco_da_Gama2.png';
-const estrela = 'https://static.vecteezy.com/system/resources/previews/001/189/080/non_2x/star-png.png';
+const estrelaPreenchida = 'https://cdn-icons-png.flaticon.com/128/118/118669.png';
+const estrelaMetade = 'https://cdn-icons-png.flaticon.com/128/25/25662.png'; 
 const iconeTrofeu = 'https://cdn-icons-png.flaticon.com/128/7466/7466210.png';
 const iconeTorcedores = 'https://cdn-icons-png.flaticon.com/128/74/74577.png';
 const iconeEstadio = 'https://cdn-icons-png.flaticon.com/128/53/53213.png';
 
 
-const Imagem = () => {
-  return(
-    <View style = {styles.imagemStyle}>
-      <Image 
-        style = {styles.formatoImagem}
-        source={{uri: imagem}}
-      />
-    </View>
-  )
-}
-const Titulo = () => {
-  return(
-    <View style = {styles.tituloStyle}>
-      <Text style = {styles.formatacaoTitulo}>Club de Regatas Vasco da Gama</Text>
-    </View>
-  )
-}
-const Texto = () =>{
-  return(
-    <View style = {styles.textoStyle}>
-      <Text>
-        O Vasco tem uma história rica e conquistou muitos títulos importantes ao longo dos anos, incluindo campeonatos nacionais e estaduais, bem como competições internacionais. 
-      </Text>
-    </View>
-
-  )
-}
-const Avaliacao = () => {
-  return(
-    <View style = {styles.avaliacaoStyle}>
-      <Image
-        style = {styles.formatacaoAvaliacao}
-        source = {{uri: estrela}}
-      />
-      <Image
-        style = {styles.formatacaoAvaliacao}
-        source = {{uri: estrela}}
-      />
-      <Image
-        style = {styles.formatacaoAvaliacao}
-        source = {{uri: estrela}}
-      />
-      <Image
-        style = {styles.formatacaoAvaliacao}
-        source = {{uri: estrela}}
-      />
-    <Image
-        style = {styles.formatacaoAvaliacao}
-        source = {{uri: estrela}}
-      />
-      <Text style = {styles.textoFormatacao}>170 Review</Text>
-    </View>
-  )
-}
-const Informacoes = () => {
-  return(
-    <View>
-    <View style = {styles.informacoesStyle}>
-    <Image
-        style = {styles.formatacaoInformacoes}
-        source = {{uri: iconeTrofeu}}
-      />
-    <Image
-        style = {styles.formatacaoInformacoes}
-        source = {{uri: iconeTorcedores}}
-      />
-    <Image
-        style = {styles.formatacaoInformacoes}
-        source = {{uri: iconeEstadio}}
-      />
-    </View>
-    <View style = {styles.textoInformacoes1}>
-      <Text>Títulos</Text>
-      <Text>Torcedores</Text>
-      <Text>Estadio</Text>
-    </View>
-    <View style = {styles.textoInformacoes2}>
-      <Text>115</Text>
-      <Text>13.292.800</Text>
-      <Text> São Januário</Text>
-    </View>
-
-    </View>
-  )
-}
-
 export default App = () => {
   return(
-    <View style = {styles.container}>
-      <Imagem/>
-      <Titulo/>
-      <Texto/>
-      <Avaliacao/>
-      <Informacoes/>
+    <View style = {{flex:1, }}>
+      <View style = {{flexDirection: 'row', justifyContent: 'center', alignItems: 'center', padding: 10}}>
+        <Image
+          style = {{width: 150, height: 110}}
+          source = {{uri: 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Vasco_da_Gama2.png'}}
+        />
+      </View>
+
+      <View style = {{flexDirection: 'row', justifyContent: 'center', alignItems: 'center', backgroundColor: 'gray'}}>
+        <Text style = {{fontSize: 20, fontWeight: 'bold', }}>
+          Vasco da Gama
+        </Text>
+      </View>
+
+      <View style = {{flexDirection: 'row', justifyContent: 'center', alignItems: 'center', textAlign: 'justify', padding: 10,}}>
+        <Text style = {{fontFamily: 'Arial'}}>
+          O Vasco tem uma história rica e conquistou muitos títulos importantes ao longo dos anos, incluindo campeonatos nacionais e estaduais, bem como competições internacionais. 
+        </Text>
+      </View>
+
+      <View style = {{flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+        <View style = {{padding: 2}}>
+        <Image
+          style = {{width: 20, height: 20}}
+          source = {{uri: estrelaPreenchida}}
+        />
+        </View>
+        <View style = {{padding: 2}}>
+        <Image
+          style = {{width: 20, height: 20}}
+          source = {{uri: estrelaPreenchida}}
+        />
+        </View>
+        <View style = {{padding: 2}}>
+        <Image
+          style = {{width: 20, height: 20}}
+          source = {{uri: estrelaPreenchida}}
+        />
+        </View>
+        <View style = {{padding: 2}}>
+        <Image
+          style = {{width: 20, height: 20}}
+          source = {{uri: estrelaPreenchida}}
+        />
+        </View>
+        <View style = {{padding: 2}}>
+        <Image
+          style = {{width: 20, height: 20}}
+          source = {{uri: estrelaMetade}}
+        />
+        </View>
+        <View  style = {{paddingLeft:20}}>
+        <Text style = {{fontWeight: 'bold'}}>
+          170 Review
+        </Text>
+        </View>
+      </View>
+
+      <View >
+        <View style = {{flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', paddingTop: 40 }}>
+          <Image
+            style = {{width: 30, height: 30,}}
+            source = {{uri: iconeTrofeu}}
+          />
+          <Image
+            style = {{width: 30, height: 30,}}
+            source = {{uri: iconeTorcedores}}
+          />
+          <Image
+            style = {{width: 30, height: 30,}}
+            source = {{uri: iconeEstadio}}
+          />
+        </View>
+        <View style = {{flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', paddingTop: 10}}>
+          <Text>Troféus</Text>
+          <Text>Torcedores</Text>
+          <Text>Estádio</Text>
+        </View>
+        <View style = {{flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', paddingTop: 10, padding: 10}}>
+        <Text>115</Text>
+        <Text>13.292.800</Text>
+        <Text>São Januário</Text>
+      </View>
+      </View>
+
     </View>
 
   )
-}
-const styles = StyleSheet.create = {
-  container: {
-    flex: 1,
-  },
-  imagemStyle: {
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: 10,
-    paddingBottom: 5,
-  },
-  formatoImagem: {
-    width: 150,
-    height: 110,
-  },
-  tituloStyle: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    paddingTop: 7,
-    paddingBottom: 7,
-  },
-  formatacaoTitulo: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  textoStyle: {
-    flexDirection: 'row',
-    textAlign: 'justify',
-    justifyContent: 'center',
-    padding: 8,
-  },
-  avaliacaoStyle: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: 15,
-  },
-  formatacaoAvaliacao: {
-    width: 20,
-    height: 20,
-    paddingTop:7
-  },
-  textoFormatacao: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    flexDirection: 'row',
-    textAlign: 'justify',
-    justifyContent: 'center',
-    paddingLeft: 13,
-  },
-  informacoesStyle: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    textAlign: 'center',
-    paddingTop: 50,
-  },
-  formatacaoInformacoes: {
-    width: 30,
-    height: 30,
-  },
-  textoInformacoes1: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    textAlign: 'center',
-    paddingTop: 10,
-    paddingLeft: 8
-  },
-    textoInformacoes2: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    textAlign: 'center',
-    paddingTop: 10,
-    paddingLeft: 18,
-  }
 }
