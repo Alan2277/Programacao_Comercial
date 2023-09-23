1acc7d0167c0ac3f2d8d4c2495f36563ffc6d37c
Alan2277 <121243339+Alan2277@users.noreply.github.com>
2023-09-23 11:28:57 -0300

    Update and rename atividadeRascunho.js to atividade07.js

---
+++ programacao-comercial/atividade07.js
@@ -0,0 +1,87 @@
+import {Text, View, StyleSheet,Image} from 'react-native';
+
+const alunos = [
+  {nome: 'Abner', media: 8},
+  {nome: 'Alan', media: 7.5},
+  {nome: 'Eliene' , media: 6 },
+  {nome: 'Francisco' , media: 8.5 },
+  {nome: 'André' , media: 5.5 },
+]
+
+const imagem = 'https://sites.ifpi.edu.br/lapesi/images/ifpilogo.png';
+
+const ListaItems = ({props}) => {
+  return(
+    <View style = {styles.estiloAlunos}>
+      <View>
+        <Text><Text style = {styles.formatacaoItems}>Nome:</Text> <Text>{props.nome}</Text></Text>
+      </View>
+
+      <View>
+        <Text><Text style = {styles.formatacaoItems}>Média:</Text> <Text>{props.media}</Text></Text>
+      </View>
+    </View>
+  )
+}
+
+const Imagem = () => {
+  return(
+    <View style = {styles.estiloImagem}>
+      <Image
+        style = {styles.formatacaoImagem}
+        source = {{uri: imagem}}
+      />
+    </View>
+  )
+}
+
+export default App = () => {
+  const lista = []
+  for (const aluno of alunos){
+    lista.push(<ListaItems props = {aluno}/>)
+  }
+
+  return(
+    <View style = {styles.container}>
+      <Imagem/>
+      <View style = {styles.estiloTexto}>
+        <Text style = {styles.formatacaoTexto}>Lista de Alunos</Text>
+      </View>
+      {lista}
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create = {
+  container: {
+    flex: 1,
+    backgroundColor: '#DCDCDC',
+    flexDirection: 'column',
+  },
+  estiloTexto: {
+    alignItems: 'center',
+    padding: 10,
+  },
+  formatacaoTexto: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'red'
+
+  },
+  estiloAlunos: {
+    paddingBottom: 10,
+    paddingLeft: 4,
+  },
+  formatacaoItems: {
+     fontSize: 16,
+     fontWeight: 'bold'
+  },
+  estiloImagem: {
+    padding:10
+  },
+  formatacaoImagem:{
+    height: 80,
+    weight: 80
+  },
+}
